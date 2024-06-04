c7cd07bd00ec8c3d35ba1744659cfa3ca6593b2b
AardJan <43217550+AardJan@users.noreply.github.com>
2024-06-04 16:04:56 +0200

    chore: meet test requirements, fix code.

--- pz_backend/users/models.py
+++ pz_backend/users/models.py
@@ -1,5 +1,24 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(username, password, **extra_fields)
+
+
 class User(AbstractUser):
-    email = None  # Usuwamy pole email
+    email = None
+
+    objects = CustomUserManager()
--- pz_backend/users/views.py
+++ pz_backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
